7f597d2712f15918f3ced3f46b49e99b382eff16
sureshvenu <suresh.venu@intel.com>
2016-03-21 14:54:24 +0530

    Managing /etc/motd

--- manifests/site.pp
+++ manifests/site.pp
@@ -43,4 +43,12 @@ node default {
   # Example:
   #   class { 'my_class': }
   notify { "Hello, my name is ${::hostname} and this is Bisi bele bath": }
+  
+  file {'/etc/motd':
+  ensure => file,
+  owner => "root",
+  group => "root",
+  mode =>  "0644",
+  content => "We learn Puppet and we are experts from this moment forward",
+  }
 }
