06e8a3504da0edcb3ee7b3f3f7c1e94a0d63afa5
sureshvenu <suresh.venu@intel.com>
2016-03-22 14:27:14 +0530

    Added missing class definition keyword :-)

--- site/memcached/manifests/init.pp
+++ site/memcached/manifests/init.pp
@@ -1,18 +1,21 @@
-file { '/etc/sysconfig/memcached.conf':
-  ensure => file,
-  owner  => 'root',
-  group  => 'root',
-  mode   => '0644',
-  source => 'puppet:///modules/memcached/memcached.conf',
-  notify => Service['memcached'],
-}
-
-service { 'memcached':
-  ensure => running,
-  enable => true,
-}
-
-package { 'memcache':
-  ensure => present,
-  before => File['/etc/sysconfig/memcached.conf'],
+class memcached {
+            
+            file { '/etc/sysconfig/memcached':
+              ensure => file,
+              owner  => 'root',
+              group  => 'root',
+              mode   => '0644',
+              source => 'puppet:///modules/memcached/memcached.cfg',
+              notify => Service['memcached'],
+            }
+            
+            service { 'memcached':
+              ensure => running,
+              enable => true,
+            }
+            
+            package { 'memcached':
+              ensure => present,
+              before => File['/etc/sysconfig/memcached.conf'],
+            }
 }
