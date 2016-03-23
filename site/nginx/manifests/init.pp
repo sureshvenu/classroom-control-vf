1209e4cdd925fd0c8fb4d6165d6795499a20e7a0
sureshvenu <suresh.venu@intel.com>
2016-03-23 10:43:12 +0530

    Refactored

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,4 +1,11 @@
 class nginx {
+
+   File {
+    owner => 'root',
+    group => 'root',
+    mode => '0644',
+  }
+  
   # package nginx
   package {'nginx': 
     ensure => present,
@@ -7,26 +14,17 @@ class nginx {
   # document root /var/www
   file {'/var/www':
     ensure => directory,
-    owner => 'root',
-    group => 'root',
-    mode => '0755',
   }
   
   # index.html
   file {'/var/www/index.html':
     ensure => file,
-    owner => 'root',
-    group => 'root',
-    mode => '0644',
     source => 'puppet:///modules/nginx/index.html',
   }
   
   # config file nginx.conf
   file {'/etc/nginx/nginx.conf':
     ensure => file,
-    owner => 'root',
-    group => 'root',
-    mode => '0644',
     source => 'puppet:///modules/nginx/nginx.conf',
     notify => Service['nginx'],
     require => Package['nginx'],
@@ -35,9 +33,6 @@ class nginx {
   # config default.conf
   file {'/etc/nginx/conf.d/default.conf':
     ensure => file,
-    owner => 'root',
-    group => 'root',
-    mode => '0644',
     source => 'puppet:///modules/nginx/default.conf',
     notify => Service['nginx'],
     require => Package['nginx'],
