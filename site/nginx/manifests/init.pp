e825d04693e78bb18f89f6e0497c9508c4449264
sureshvenu <suresh.venu@intel.com>
2016-03-24 08:48:49 +0530

    Updated with OS specific paramaeters.

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -5,45 +5,60 @@ class nginx {
                      $confdir = '/etc/nginx'
                      $logdir  = '/var/log/nginx'
                      $nginxrunasuser   = 'nginx'
+                     $package_name = 'nginx'
+                     $file_owner = 'root'
+                     $file_group = 'root'
+                     $docroot = '/var/www'
+                     
                }
                'debian' :{
-               $confdir = '/etc/nginx'
+                     $confdir = '/etc/nginx'
                      $logdir  = '/var/log/nginx'
                      $nginxrunasuser   = 'www-data'
+                     $package_name = 'nginx'
+                     $file_owner = 'root'
+                     $file_group = 'root'
+                     $docroot = '/var/www'
                }
                'windows' :{
                      $confdir = 'C:/ProgramData/nginx/html'
                      $logdir  = 'C:/ProgramData/nginx/logs'
                      $nginxrunasuser   = 'nobody'
+                     $package_name = 'nginx-service'
+                     $file_owner = 'administrator'
+                     $file_group = 'administrators'
+                     $docroot = '/var/www'
+                     
                }
                'default' :{
                      fail("Unsupported OS nginx install config failed on ${::osfamily}")
                }
    }
    File {
-    owner => 'root',
-    group => 'root',
+    owner => $file_owner,
+    group => $file_group,
     mode => '0644',
   }
   
   # package nginx
   package {'nginx': 
     ensure => present,
+    name => ${package_name},
   }
   
   # document root /var/www
-  file {'/var/www':
+  file {${docroot}:
     ensure => directory,
   }
   
   # index.html
-  file {'/var/www/index.html':
+  file {"${docroot}/index.html":
     ensure => file,
     source => 'puppet:///modules/nginx/index.html',
   }
   
   # config file nginx.conf
-  file {'/etc/nginx/nginx.conf':
+  file {"${confdir}/nginx.conf":
     ensure => file,
     source => 'puppet:///modules/nginx/nginx.conf',
     notify => Service['nginx'],
@@ -51,7 +66,7 @@ class nginx {
   }
   
   # config default.conf
-  file {'/etc/nginx/conf.d/default.conf':
+  file {"${confdir}/conf.d/default.conf":
     ensure => file,
     source => 'puppet:///modules/nginx/default.conf',
     notify => Service['nginx'],
